39bf1491e588dc4413297f3ab36a4c77518ea296
Robert Byrne <bobbyrne01@gmail.com>
2015-01-14 17:14:48 +0000

    directory re-structuring

--- addon/lib/ToggleButton.js
+++ addon/lib/ToggleButton.js
@@ -1,21 +1,23 @@
-var { ToggleButton } = require("sdk/ui/button/toggle"),
+var {
+	ToggleButton
+} = require("sdk/ui/button/toggle"),
 	Data = require("./Data"),
 	Panel = require("./Panel"),
 	button;
 
-exports.init = function() {
-	
+exports.init = function () {
+
 	button = ToggleButton({
 		id: "timekeeper-widget",
 		label: 'Time Keeper',
 		icon: Data.get("images/ico-64.png"),
 		onChange: handleChange
 	});
-}
+};
 
-exports.get = function() {
+exports.get = function () {
 	return button;
-}
+};
 
 function handleChange(state) {
 	if (state.checked) {
